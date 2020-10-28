eaaccb1b9bc1aa28004fdb22d7b342ddb5f57b07
Mario-Leander Reimer <Mario-Leander.Reimer@partner.bmw.de>
2020-10-28 15:20:45 +0100

    Removed interpolation.

--- microk8s-gcloud/outputs.tf
+++ microk8s-gcloud/outputs.tf
@@ -1,7 +1,7 @@
 output "master-ip" {
-    value = "${google_compute_instance.microk8s-master.network_interface.0.access_config.0.nat_ip}"
+    value = google_compute_instance.microk8s-master.network_interface.0.access_config.0.nat_ip
 }
 
 output "node-ip" {
-    value = "${google_compute_instance.microk8s-node.network_interface.0.access_config.0.nat_ip}"
+    value = google_compute_instance.microk8s-node.network_interface.0.access_config.0.nat_ip
 }
